3ac38320b74a22f8cbd6d492adbb1b4a26a8578c
Ben Cho <ben.cho@gmail.com>
2021-08-15 23:36:55 -0600

    added docstring_from decorator

--- src/energyderivatives/__init__.py
+++ src/energyderivatives/__init__.py
@@ -4,3 +4,16 @@ from .vanillaoptions import *
 from . import basic_american_options
 from . import binomial_tree_options
 from . import heston_nandi_options
+
+
+def docstring_from(source):
+    """
+    Decorator to be used to copy the docstring from the source class/class method
+    to the target. Useful for class composition.
+    """
+
+    def wrapper(target):
+        target.__doc__ = source.__doc__
+        return target
+
+    return wrapper
